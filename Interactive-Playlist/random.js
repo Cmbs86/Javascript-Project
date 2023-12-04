3ffba6353a6a0a5b51fe796db2cdf58bdd5a6a2d
Carlos Sousa <carlosmanuel.desousa@digitalcareerinstitute.org>
2023-12-04 20:24:00 +0100

    end of day commit

--- Interactive-Playlist/random.js
+++ Interactive-Playlist/random.js
@@ -1,4 +1,4 @@
-function Newthing(a, b, c) {
+/* function Newthing(a, b, c) {
   console.log (a + b + c)
 }
 
@@ -13,3 +13,41 @@ function another(){
 
 
 console.log(another(Newthing()))
+ */
+
+
+
+const array = [1,5,6,4,23,4,5,2,4]
+
+const newArray = array.map((number) => {
+    return number * 2
+})
+
+
+console.log(array)
+console.log(newArray)
+// const newArray = []
+// for(let i = 0; i < array.length; i++){
+//     newArray.push(array[i] * 2)
+// }
+
+// console.log(array)
+// console.log(newArray)
+
+
+// const array2 = [1,5,6,4,23,4,5,2,4]
+// const newArray2 = array2.filter((number) => {
+//     return number != 4;
+// })
+
+
+
+// console.log(array2)
+// console.log(newArray2)
+
+
+
+// const array2 = [1,5,6,4,23,4,5,2,4]
+
+
+
